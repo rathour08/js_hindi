a3d00a7a2b50a56131813cc4dba00ea3422c10ef
rathour08 <104031015+rathour08@users.noreply.github.com>
2023-10-03 07:26:36 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+console.log("2" > 1);// automatically 2 is converted in number
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// comparison operator convert null to zero
+// equality is different form comparison in javascript
+// strict check ===
+console.log("2"===2)
+//avoid conversion like these
