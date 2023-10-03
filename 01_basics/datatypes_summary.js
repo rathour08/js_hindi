5a290e3d84319d34c91a0f9d5471d1be91ad618c
rathour08 <104031015+rathour08@users.noreply.github.com>
2023-10-03 07:41:44 +0000

    summary of datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,21 @@
+// primitive data types
+// 7 types: these are call by value 
+// string, number, boolean, null, undefined, symbol, bigint
+
+// java script is dynamically typed language
+ const score = 100 // Number
+ const scorevalue = 100.3 // number
+ const isloggedin = false // Boolean
+ let username; // undefined
+
+ const id = Symbol('123')
+ const bignumber = 2349579379375n
+
+
+
+// reference (Non premitive)
+// Arrays, Objects, Functions
+const heros = ["shaktiman", "doga"];
+const myfuction = function(){
+    console.log("Hello Sahil");
+}
