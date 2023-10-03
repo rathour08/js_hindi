09057e57c037dc8143ffe6af14a8afe447ec7d65
rathour08 <104031015+rathour08@users.noreply.github.com>
2023-10-03 06:57:53 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,19 @@
+let score = "33abc"
+
+console.log(typeof(score));
+let valueinnumber= Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+// "33" => 33
+//"33abc" => NaN
+// ture => 1
+// false => 0
+// null => 0
+//Boolean
+// 1=> true
+//0=> false
+//"sahil" =>true
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringnumber);
