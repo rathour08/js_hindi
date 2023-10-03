c41af73d5807a0a8ffac2be075adeae91dd8a70f
rathour08 <104031015+rathour08@users.noreply.github.com>
2023-10-03 06:21:30 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 144553
+let accountEmail="sahil"
+var accountPassword="1234"
+accountCity = "Jaipur"
+accountEmail ="hc@fj"
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // we are using node not browser
+
+let name = "sahil"
+let age = 24
+let isloggedin = false
+// number => 2 to power 53
+// bigint
+// string => ""
+//boolean => true/false
+// null => standalone value(empty value ka representation)
+// undefined => value not defined
+// symbol => unique
+
+//object
+
+console.log(typeof "sahil");
+console.log(typeof undefined); // undefined
+console.log(typeof null)// object
