af36a2d92a8cbb4665f1000b8ffcc858d1113a86
rathour08 <104031015+rathour08@users.noreply.github.com>
2023-10-03 07:14:29 +0000

    moreoncon

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -1,9 +1,9 @@
 let score = "33abc"
 
-console.log(typeof(score));
+// console.log(typeof(score));
 let valueinnumber= Number(score)
-console.log(typeof valueinnumber);
-console.log(valueinnumber);
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);
 // "33" => 33
 //"33abc" => NaN
 // ture => 1
@@ -15,5 +15,18 @@ console.log(valueinnumber);
 //"sahil" =>true
 let somenumber = 33
 let stringnumber = String(somenumber)
-console.log(stringNumber);
-console.log(typeof stringnumber);
+// console.log(stringNumber);
+// console.log(typeof stringnumber);
+//******operations ******
+let value =3
+let negvalue = -value
+// console.log(negvalue);
+let str1 = "hello"
+let str2 = " sahil"
+let str3 = str1+str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2 + "2");
+
