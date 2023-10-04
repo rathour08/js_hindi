063e4833e8edf115cdd9be2f6dc8830bbbbc3dc7
rathour08 <104031015+rathour08@users.noreply.github.com>
2023-10-04 05:50:58 +0000

    Math and number

---
+++ 01_basics/05_string.js
@@ -0,0 +1,19 @@
+const name = "sahil"
+const repocount =50
+// console.log(name + repocount ); //outdated syntax
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+const gameName = new String("sahilrathour")
+console.log(gameName[0]);
+console.log(gameName.length)
+console.log(gameName.indexOf("i"));
+let newstring= gameName.substring(0,4)
+console.log(newstring);
+let anothersting = gameName.slice(-8, 4)
+console.log(anothersting);
+let stringone= "   sahil"
+let stringtwo= stringone.trim();
+console.log(stringone);
+console.log(stringtwo);
+
+
+
---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,31 @@
+const score=400;
+// console.log(score);
+
+const balance =new Number(100);
+
+console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+const othernumber = 123.8966;
+// console.log(othernumber.toPrecision(3));
+const hunderds = 1000000;
+// console.log(hunderds.toLocaleString('en-IN'));
+//++++++++++++++++ Maths ++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,5,7,6,9));
+// console.log(Math.random()); // gives value between 0 and 1
+// console.log((Math.random()*10)+1);
+
+const min =10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min +1))+min);
+
+
+
+
+
