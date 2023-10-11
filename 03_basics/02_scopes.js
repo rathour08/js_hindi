e9732295a8bb9c31f5f02bdcebecb87df47061ff
rathour08 <104031015+rathour08@users.noreply.github.com>
2023-10-11 16:29:31 +0000

    functions

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,30 @@
+function one(){
+    const username = "sahil"
+
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+    //console.log(website);
+    //two();
+}
+//one();
+
+
+//++++++++++ intersting ++++++
+
+function addone(num){
+    return num + 1
+}
+
+addone(5)
+
+
+
+const addtwo = function(num){
+    return num + 2
+}
+
+addtwo(5)
+
+//function in dono tariko se bn skte hai javascript m variables kuch be hold kr skte hai
