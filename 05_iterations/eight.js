bf44e7e6543070e43605d90229b62f9f16eaeca7
rathour08 <104031015+rathour08@users.noreply.github.com>
2023-10-13 11:55:13 +0000

    reducer function

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,4 +1,4 @@
-const mynums = [1,2,3];
+// const mynums = [1,2,3];
 
 // const mytotal=mynums.reduce( function (acc, currval) {
 //     console.log(`acc: ${acc} and currval: ${currval}`);
@@ -7,6 +7,28 @@ const mynums = [1,2,3];
 
 // using arrow function
 
-const mytotal = mynums.reduce( (acc, currval) => acc+currval , 0)
+// const mytotal = mynums.reduce( (acc, currval) => acc+currval , 0)
+
+// console.log(mytotal);
+
+
+const shopptingcart = [
+    {
+        itemname: "js course",
+        price: 2999
+    },
+    {
+        itemname: "py course",
+        price: 999
+
+    },
+    {
+        itemname: "mobile dev",
+        price: 2999
+
+    }
+
+]
+const mytotal = shopptingcart.reduce( (acc, currval) => currval.price + acc, 0 );
 
 console.log(mytotal);
