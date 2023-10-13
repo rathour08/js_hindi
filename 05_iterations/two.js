7d9232b574c7b511bde9d28365c6d1af6b56dd3d
rathour08 <104031015+rathour08@users.noreply.github.com>
2023-10-13 07:06:07 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,17 @@
+// while
+// let i=0;
+
+// while(i<10){
+//     console.log(`value is ${i}`);
+//     i++;
+
+// }
+
+// do while
+
+// let score =11;
+// do {
+//     console.log(`score is ${score}`);
+//     score ++;
+    
+// } while (score <= 10);
