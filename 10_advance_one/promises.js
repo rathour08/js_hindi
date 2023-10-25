25931b30ef6f1a8dd0dd4bc48119c433535da28b
rathour08 <104031015+rathour08@users.noreply.github.com>
2023-10-25 04:57:56 +0000

    commit

---
+++ 10_advance_one/promises.js
@@ -0,0 +1,95 @@
+const promiseOne = new Promise(function (resolve, reject) {
+    // do an asyn task
+    // db calls, network calls
+
+    setTimeout(() => {
+
+        console.log("async task is completed")
+        resolve();
+        
+    }, 1000);
+    
+});
+
+promiseOne.then(function(){
+    console.log("promise consumed")
+
+});
+
+new Promise(function(resolve, reject){
+    setTimeout(() => {
+        console.log("promise done")
+        resolve();
+        
+    }, 1000);
+
+}).then(function(){
+    console.log("promise is kept");
+
+})
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        resolve({userneme:"sahil", lastname: "rathour"})
+        
+    }, 1000);
+})
+
+promiseThree.then(function(user){
+    console.log(user.userneme);
+
+})
+
+
+const promisefour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username: "sahil", email: "sahil@124"})
+        }
+        else{
+            reject("something went wrong")
+        }
+
+    },1000)
+})
+
+promisefour.then(function(user){
+    console.log(user);
+    return user.username;
+
+}).then(function(username){
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(function(){
+    console.log("the promise is either resolved or rejected")
+})
+
+
+const promisefive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username:"sahil", lastname:"rathour"})
+        }
+        else{
+            reject("something went wrong")
+        }
+    },1000)
+})
+
+async function consumePromiseFive(){
+    try {
+        const response = await promisefive;
+        console.log(response);
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+consumePromiseFive();
+
+
